4ea4f2da77f758a6422331633a62dffe5f7c3b7a
Napoleon Wagnerson <107638402+papi-DarknesHt@users.noreply.github.com>
2026-06-20 21:33:34 -0400

    updateclient pour production

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://rekolthtbackend.onrender.com" ?? "http://localhost:8000";
 
 async function request(path, options = {}) {
   // recupere le token du localStorage  
